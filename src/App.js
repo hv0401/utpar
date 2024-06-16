b659f5b4067ae403c276a24e9dd80f6b24e5f24b
hv0401 <dwivedibobby@gmail.com>
2024-06-16 15:11:49 +0530

    route fix

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import AboutUs from './components/AboutUs/AboutUs';
 import Home from './components/Home/Home'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, HashRouter } from 'react-router-dom';
 import Body from './components/Body/Body';
 import Services from './components/Services/Services';
 import Resources from './components/Resources/Resources';
@@ -12,13 +12,15 @@ import Resources from './components/Resources/Resources';
 function App() {
   return (
     <>
-      <Header/>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/aboutus" element={<AboutUs />} />
-        <Route path='/services' element={<Services />} />
-        <Route path='/resources' element={<Resources />} />
-      </Routes>
+      <Header />
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/aboutus" element={<AboutUs />} />
+          <Route path='/services' element={<Services />} />
+          <Route path='/resources' element={<Resources />} />
+        </Routes>
+      </HashRouter>
       <Footer />
       {/* <div style={{ width: '100vw', marginTop: '-20px', padding: '0' }}>
         <Header className="sticky-header" />
